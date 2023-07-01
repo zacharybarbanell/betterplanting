4ffcdf2d73aac00200db204d85eb9a88a73ade75
Zachary Barbanell <zachgalaxy@comcast.net>
2023-07-01 10:59:20 -0600

    implement parser for new config system

---
+++ src/main/java/com/zacharybarbanell/betterplanting/config/Parser.java
@@ -0,0 +1,37 @@
+package com.zacharybarbanell.betterplanting.config;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.function.Function;
+
+import com.google.common.base.Functions;
+import com.google.common.collect.ImmutableMap;
+
+public class Parser {
+    private Parser() {}
+
+    private static final Map<Class<?>, Function<String, ?>> PARSERS = ImmutableMap.<Class<?>, Function<String, ?>>builder()
+        .put(String.class, Functions.identity())
+        .put(Double.class, Double::parseDouble)
+        .put(Integer.class, Integer::valueOf) 
+        .put(Boolean.class, Boolean::valueOf)
+        .build();
+
+    public static <T> Optional<T> parse(String s, Class<T> clazz) {
+        if (!PARSERS.containsKey(clazz)) {
+            throw new IllegalArgumentException(String.format("Type %s has no parser", clazz));
+        }
+        else {
+            Function<String, T> func = (Function<String, T>) PARSERS.get(clazz);    
+            T result;
+            try {
+                result = func.apply(s);
+            }
+            catch (Exception e) {
+                return Optional.empty(); //error gets logged upstream
+            }
+            return Optional.of(result);
+        }
+    }
+}
